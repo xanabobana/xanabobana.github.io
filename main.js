dc09d088b97210fa8ff821d233113841678f5d4f
Xana Wolf <xanabobana@yahoo.com>
2020-06-03 09:07:57 -0400

    final version 1

---
+++ main.js
@@ -0,0 +1,50 @@
+document.getElementById("theatrical").addEventListener("click", theatrical);
+document.getElementById("professional").addEventListener("click", professional);
+document.getElementById("stained_glass").addEventListener("click", stained_glass);
+
+child=document.getElementById('banner_hover');
+node=document.getElementById('banner');
+
+function theatrical() {
+    let hasChild = node.querySelector("#banner_hover");
+    if (hasChild === null) {
+        node.appendChild(child);
+    }
+    let list_element = document.getElementById('theatrical');
+    list_element.className="current_page_item";
+    document.getElementById('professional').className='';
+    document.getElementById('stained_glass').className='';
+    document.getElementById('banner_img').src="images/theaterresume.jpg";
+    child.href="files/theaterresume.pdf";
+    document.getElementById('social_links').style.display = "none";
+    document.getElementById('glass_links').style.display = "none";
+}
+
+function professional() {
+    let hasChild = node.querySelector("#banner_hover");
+    if (hasChild === null) {
+        node.appendChild(child);
+    }
+    let list_element = document.getElementById('professional');
+    list_element.className="current_page_item";
+    document.getElementById('theatrical').className='';
+    document.getElementById('stained_glass').className='';
+    document.getElementById('banner_img').src="images/AlexanaResume.jpg";
+    child.href="files/AlexanaResumeBCA.pdf";
+    document.getElementById('glass_links').style.display = "none";
+    document.getElementById('social_links').style.display = "block";
+
+
+}
+
+function stained_glass() {
+    let list_element = document.getElementById('stained_glass');
+    list_element.className="current_page_item";
+    document.getElementById('professional').className='';
+    document.getElementById('theatrical').className='';
+    document.getElementById('glass_links').style.display = "block";
+    document.getElementById('social_links').style.display = "none";
+    document.getElementById('banner_img').src="images/BeFunky-collage.jpg";
+    node.removeChild(child);
+
+}
