8c3b3a53baa81f8be413701b9d57abad5a25f0be
Xana Wolf <xanabobana@yahoo.com>
2020-06-03 23:22:37 -0400

    glass site update

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ document.getElementById("stained_glass").addEventListener("click", stained_glass
 child=document.getElementById('banner_hover');
 node=document.getElementById('banner');
 
+let img=new Image();
+img.src='images/BeFunky-collage.jpg';
+
+
 function theatrical() {
     let hasChild = node.querySelector("#banner_hover");
     if (hasChild === null) {
@@ -18,9 +22,11 @@ function theatrical() {
     child.href="files/theaterresume.pdf";
     document.getElementById('social_links').style.display = "none";
     document.getElementById('glass_links').style.display = "none";
+
 }
 
 function professional() {
+
     let hasChild = node.querySelector("#banner_hover");
     if (hasChild === null) {
         node.appendChild(child);
@@ -33,18 +39,21 @@ function professional() {
     child.href="files/AlexanaResumeBCA.pdf";
     document.getElementById('glass_links').style.display = "none";
     document.getElementById('social_links').style.display = "block";
-
-
 }
 
 function stained_glass() {
+    banner=document.getElementById("banner_img");
     let list_element = document.getElementById('stained_glass');
     list_element.className="current_page_item";
     document.getElementById('professional').className='';
     document.getElementById('theatrical').className='';
     document.getElementById('glass_links').style.display = "block";
     document.getElementById('social_links').style.display = "none";
-    document.getElementById('banner_img').src="images/BeFunky-collage.jpg";
+    banner.src="images/BeFunky-collage.jpg";
     node.removeChild(child);
-
+    var a=document.createElement('a');
+    a.href='/glass/index.html';
+    b= a.appendChild(banner);
+document.getElementById('banner').appendChild(a);
 }
+
